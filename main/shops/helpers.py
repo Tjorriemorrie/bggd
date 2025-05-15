75a5623431a3a4d567a6912aad55812155fafd46
Tjorriemorrie <jacoj82@gmail.com>
2025-05-15 13:31:20 +0200

    removing spaces

--- main/shops/helpers.py
+++ main/shops/helpers.py
@@ -136,7 +136,7 @@ def parse_price(price_txt) -> float:
     match = re.search(r'R\s?([\d,]+(\.\d{2})?)', price_txt)
     if not match:
         raise ValueError(f'Could not extract price: {price_txt}')
-    price = float(match.group(1).replace(',', ''))
+    price = float(match.group(1).replace(',', '').replace(' ', ''))
     return price
 
 
