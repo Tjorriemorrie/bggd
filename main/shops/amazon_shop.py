41c93e5a662efe2cd91c0b3e52f748870593c1f6
Tjorriemorrie <jacoj82@gmail.com>
2026-03-30 19:31:43 +0200

    amazon

--- main/shops/amazon_shop.py
+++ main/shops/amazon_shop.py
@@ -1,42 +1,48 @@
 import logging
 import re
 
+from botasaurus_requests import request as bot_request
 from bs4 import BeautifulSoup
 
-from main.games import get
 from main.selectors import upsert_shop
 from main.shops.helpers import handle_item_data, missed_listings, parse_price
 
 logger = logging.getLogger(__name__)
 
-enabled = False
+enabled = True
 shop_name = 'Amazon'
 shop_host = 'https://www.amazon.co.za'
+# https://www.amazon.co.za/s?i=toys&rh=n:28002628031,p_72:28056829031,p_6:A34KVLZUJN6MA,p_n_availability:28056815031&dc=&page=1
 
 
 def worker(page: int) -> bool:
     """Scrape page."""
     logger.info(f' Scraping page {page} '.center(99, '='))
     shop = upsert_shop(shop_name)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0',  # noqa E501
-    }
     url = f'{shop_host}/s'
     params = {
         'i': 'toys',
         'rh': 'n:28002628031,p_72:28056829031,p_6:A34KVLZUJN6MA,p_n_availability:28056815031',
         'dc': '',
-        # 'ds': 'v1:YzAW0qEQtL1K5oouJPBjxnT3d2hYH3OjHPuLt6K5EZ0',
-        'page': page,
-        # 'content-id': 'amzn1.sym.7738ade6-b071-4ce1-991e-a3a167042a7a',
+        'page': str(page),
     }
-    res = get(url, params=params, headers=headers)
-    logger.info(f'Scraped {res.request.url}...')
+    query = '&'.join(f'{k}={v}' for k, v in params.items())
+    full_url = f'{url}?{query}'
+
+    res = bot_request.get(full_url, headers={})
+    logger.info(f'Scraped {full_url} (status {res.status_code})...')
+    ok = 200
+    if res.status_code != ok:
+        logger.warning(f'Got status {res.status_code}')
+        return False
     if 'Try checking your spelling or use more general terms' in res.text:
         return False
 
     html = BeautifulSoup(res.text, 'html.parser')
     container = html.find('div', class_='s-result-list')
+    if not container:
+        logger.warning('No result list container found')
+        return False
     rows = container.find_all('div', class_='s-result-item')
     if not rows:
         return False
@@ -44,10 +50,15 @@ def worker(page: int) -> bool:
         min_txt_len = 100
         if len(row.text) < min_txt_len:
             continue
-        img_src = row.find('img', class_='s-image')['src']
-        href = shop_host + row.find('a', class_='a-link-normal')['href']
+        img_tag = row.find('img', class_='s-image')
+        link_tag = row.find('a', class_='a-link-normal')
+        h2_tag = row.find('h2')
+        if not img_tag or not link_tag or not h2_tag:
+            continue
+        img_src = img_tag['src']
+        href = shop_host + link_tag['href']
         href = re.sub(r'/ref.*$', '', href)
-        name = row.find('h2').get_text(separator=' ', strip=True)
+        name = h2_tag.get_text(separator=' ', strip=True)
         # price details
         in_stock = True
         try:
@@ -78,9 +89,13 @@ def worker_wrapper(page):
 
 def scrape_site():
     """Scrape pages."""
+    max_pages = 50
     page = 0
     while True:
         page += 1
+        if page > max_pages:
+            logger.info(f'Reached max pages ({max_pages}), stopping.')
+            break
         outcome = worker(page)
         if not outcome:
             break
